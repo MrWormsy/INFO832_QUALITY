8137974c22633321a4cbc977fcaf7a62f1a6e201
NanoClem <clemdecoop.pro@gmail.com>
2020-05-13 22:19:21 +0200

    documenting

--- src/discreteBehaviorSimulator/Clock.java
+++ src/discreteBehaviorSimulator/Clock.java
@@ -120,7 +120,7 @@ public class Clock {
 
 	/**
 	 * <p>Get the current {@link time Clock#time} on the clock</p>
-	 * @return current time on the clock as ticks if its virtual, else the current real time
+	 * @return current time on the clock as ticks if its virtual, else the current real time in timestamp
 	 */
 	public long getTime() {
 		if(this.virtual) {
